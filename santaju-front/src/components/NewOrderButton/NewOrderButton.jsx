f59b300c60dd8dbf481a8b30b5a13d5878098d81
Matheus Tanaka <matheustanakanog@gmail.com>
2023-01-19 19:14:54 -0300

    feat: create new order button

---
+++ santaju-front/src/components/NewOrderButton/NewOrderButton.jsx
@@ -0,0 +1,9 @@
+import styles from "./NewOrderButton.module.css";
+
+export default function NewOrderButton() {
+  return (
+    <div>
+      <button className={styles.createUser}>Novo Pedido</button>
+    </div>
+  );
+}
