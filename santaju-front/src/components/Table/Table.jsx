291ddccee7a61d511e4b37b53d554e95cedaaa37
Matheus Tanaka <matheustanakanog@gmail.com>
2023-01-20 14:29:37 -0300

    refactor: keep outisde fetchClients function outside useEffect

--- santaju-front/src/components/Table/Table.jsx
+++ santaju-front/src/components/Table/Table.jsx
@@ -2,19 +2,23 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import styles from "./Table.module.css";
 
+import EditIcon from "../EditIcon/EditIcon";
+import DeleteIcon from "../DeleteIcon/DeleteIcon";
+
 export default function Table() {
   const [clients, setClients] = useState([]);
 
+  const fetchClients = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/clients");
+      setClients(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/api/clients");
-        setClients(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
+    fetchClients();
   }, []);
 
   return (
@@ -22,12 +26,16 @@ export default function Table() {
       <tr>
         <th>Nome</th>
         <th>Telefone</th>
-        <th>Produto</th>
+        <th></th>
       </tr>
       {clients.map((client) => (
         <tr key={client.id}>
           <td>{client.name}</td>
           <td>{client.phone}</td>
+          <td className={styles.tableIcon}>
+            <EditIcon client={client} fetchClients={fetchClients} />
+            <DeleteIcon client={client} fetchClients={fetchClients} />
+          </td>
         </tr>
       ))}
     </table>
