689063d347aa85fd1aed7ea1f2435e33bae8dfee
Matheus Tanaka <matheustanakanog@gmail.com>
2023-01-19 22:17:18 -0300

    feat: add function to open modal

--- santaju-front/src/components/NewOrderButton/NewOrderButton.jsx
+++ santaju-front/src/components/NewOrderButton/NewOrderButton.jsx
@@ -1,9 +1,17 @@
 import styles from "./NewOrderButton.module.css";
 
+import { useState } from "react";
+
+import NewOrderModal from "../NewOrderModal/NewOrderModal";
+
 export default function NewOrderButton() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div>
-      <button className={styles.createUser}>Novo Pedido</button>
+      <button onClick={() => setOpenModal(true)} className={styles.createUser}>
+        Novo Pedido
+      </button>
+      <NewOrderModal open={openModal} onClose={() => setOpenModal(false)} />
     </div>
   );
 }
