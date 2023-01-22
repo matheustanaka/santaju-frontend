53b179732c0395237fcdd2076308dbde79074568
matheustanaka <matheustanakanog@gmail.com>
2023-01-22 20:45:57 -0300

    feat: set properties as default on form

--- santaju-front/src/components/NewOrderButton/NewOrderButton.jsx
+++ santaju-front/src/components/NewOrderButton/NewOrderButton.jsx
@@ -2,16 +2,27 @@ import styles from "./NewOrderButton.module.css";
 
 import { useState } from "react";
 
+import { useClient } from "../../hooks/useClient";
+
 import NewOrderModal from "../NewOrderModal/NewOrderModal";
 
 export default function NewOrderButton() {
   const [openModal, setOpenModal] = useState(false);
+
+  const { name, phone, setName, setPhone } = useClient();
+
+  const onClose = () => {
+    setName("");
+    setPhone("");
+    setOpenModal(false);
+  };
+
   return (
     <div>
       <button onClick={() => setOpenModal(true)} className={styles.createUser}>
         Novo Pedido
       </button>
-      <NewOrderModal open={openModal} onClose={() => setOpenModal(false)} />
+      <NewOrderModal open={openModal} onClose={onClose} />
     </div>
   );
 }
