c3f01ecd0c62eea9afb57487668afb4269e0a49d
matheustanaka <matheustanakanog@gmail.com>
2023-01-30 18:54:02 -0300

    feat: create project routes

--- santaju-front/src/App.jsx
+++ santaju-front/src/App.jsx
@@ -6,16 +6,27 @@ import Header from "./components/Header/Header";
 import ClientProvider from "./hooks/useClient";
 import ProductProvider from "./hooks/useProduct";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Client from "./pages/Client/Client";
+import Product from "./pages/Product/Product";
+import Order from "./pages/Order/Order";
+
 function App() {
   return (
-    <ClientProvider>
-      <ProductProvider>
-        <>
-          <Header />
-          <Dashboard />
-        </>
-      </ProductProvider>
-    </ClientProvider>
+    <BrowserRouter>
+      <ClientProvider>
+        <ProductProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/client" element={<Client />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/order" element={<Order />} />
+          </Routes>
+        </ProductProvider>
+      </ClientProvider>
+    </BrowserRouter>
   );
 }
 
