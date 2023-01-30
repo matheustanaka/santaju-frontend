e634373a41a457007b46a457d5f633c45ec5783a
matheustanaka <matheustanakanog@gmail.com>
2023-01-30 18:55:58 -0300

    feat: add Delete Icon and change color

---
+++ santaju-front/src/components/ClientDeleteIcon/DeleteIcon.jsx
@@ -0,0 +1,24 @@
+import { RiDeleteBin7Line } from "react-icons/ri";
+import axios from "axios";
+import { useClient } from "../../hooks/useClient";
+import styles from "./DeleteIcon.module.css";
+
+export default function DeleteIcon({ client, fetchClients }) {
+  const handleDeleteClient = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/api/client/${client.id}`);
+
+      await fetchClients();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <RiDeleteBin7Line
+        className={styles.deleteIcon}
+        onClick={handleDeleteClient}
+      />
+    </div>
+  );
+}
