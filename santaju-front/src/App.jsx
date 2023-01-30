809a8669c9b311ab9e1c5c60cfc7ff3ac58ed8c9
matheustanaka <matheustanakanog@gmail.com>
2023-01-30 19:29:46 -0300

    refactor: remove unused imports

--- santaju-front/src/App.jsx
+++ santaju-front/src/App.jsx
@@ -1,8 +1,5 @@
 import "./styles.css";
 
-import Dashboard from "./components/Dashboard/Dashboard";
-import Header from "./components/Header/Header";
-
 import ClientProvider from "./hooks/useClient";
 import ProductProvider from "./hooks/useProduct";
 
