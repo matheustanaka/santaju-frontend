b3e9cb407db7feacfc708e20e8ea540ec34d87a4
matheustanaka <matheustanakanog@gmail.com>
2023-01-31 19:05:27 -0300

    feat: add onClick and onClose function

--- santaju-front/src/components/CreateProductButton/CreateProductButton.jsx
+++ santaju-front/src/components/CreateProductButton/CreateProductButton.jsx
@@ -1,18 +1,29 @@
 import styles from "./CreateProductButton.module.css";
 import ProductModal from "../ProductModal/ProductModal";
 import { useState } from "react";
+import { useProduct } from "../../hooks/useProduct";
 
 export default function CreateProductButton() {
+  const { setTitle, setPrice } = useProduct();
   const [openModal, setOpenModal] = useState(false);
+
+  const onClickButton = () => {
+    setTitle("");
+    setPrice("");
+    setOpenModal(true);
+  };
+
+  const onClose = () => {
+    setTitle("");
+    setPrice("");
+    setOpenModal(false);
+  };
   return (
     <div>
-      <button
-        onClick={() => setOpenModal(true)}
-        className={styles.createProduct}
-      >
+      <button onClick={onClickButton} className={styles.createProduct}>
         Cadastrar Produto
       </button>
-      <ProductModal open={openModal} onClose={() => setOpenModal(false)} />
+      <ProductModal open={openModal} onClose={onClose} />
     </div>
   );
 }
