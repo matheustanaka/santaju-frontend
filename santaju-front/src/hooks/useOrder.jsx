15a2d4eb58fa93fb68b88052663e4abcb008dae0
matheustanaka <matheustanakanog@gmail.com>
2023-02-01 22:20:33 -0300

    feat: add order context

---
+++ santaju-front/src/hooks/useOrder.jsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+
+const OrderContext = createContext();
+
+export default function OrderProvider({ children }) {
+  const [id_client, setId_Client] = useState(0);
+  const [id_product, setId_Product] = useState(0);
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:3000/api/orders");
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <OrderContext.Provider
+      value={{
+        id_client,
+        setId_Client,
+        id_product,
+        setId_Product,
+        orders,
+        setOrders,
+        fetchOrders,
+      }}
+    >
+      {children}
+    </OrderContext.Provider>
+  );
+}
+
+export function useOrder() {
+  const context = useContext(OrderContext);
+  const {
+    id_client,
+    setId_Client,
+    id_product,
+    setId_Product,
+    orders,
+    setOrders,
+    fetchOrders,
+  } = context;
+  return {
+    id_client,
+    setId_Client,
+    id_product,
+    setId_Product,
+    orders,
+    setOrders,
+    fetchOrders,
+  };
+}
