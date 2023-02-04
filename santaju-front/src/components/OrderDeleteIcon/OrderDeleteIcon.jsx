03978e9683e904f34d128fb5bfd0e8b39de7faf6
matheustanaka <matheustanakanog@gmail.com>
2023-02-04 12:26:01 -0300

    feat: add fetch requests to update UI

--- santaju-front/src/components/OrderDeleteIcon/OrderDeleteIcon.jsx
+++ santaju-front/src/components/OrderDeleteIcon/OrderDeleteIcon.jsx
@@ -2,14 +2,20 @@ import { RiDeleteBin7Line } from "react-icons/ri";
 import axios from "axios";
 import styles from "./OrderDeleteIcon.module.css";
 import { useOrder } from "../../hooks/useOrder";
+import { useProduct } from "../../hooks/useProduct";
+import { useClient } from "../../hooks/useClient";
 
 export default function OrderDeleteIcon({ order }) {
   const { fetchOrders } = useOrder();
+  const { fetchProducts } = useProduct();
+  const { fetchClients } = useClient();
   const handleDeleteClient = async () => {
     try {
       await axios.delete(`http://localhost:3000/api/order/${order.id}`);
 
       await fetchOrders();
+      await fetchClients();
+      await fetchProducts();
     } catch (error) {
       console.log(error);
     }
